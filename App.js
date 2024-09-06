4ba786627bb1e127a6d5085bd467808e3e000dda
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2024-09-06 15:39:40 +0200

    More & more changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! test test</Text>
+      <Text>Open up App.js to start working on your app! test test test</Text>
       <StatusBar style="auto" />
     </View>
   );
